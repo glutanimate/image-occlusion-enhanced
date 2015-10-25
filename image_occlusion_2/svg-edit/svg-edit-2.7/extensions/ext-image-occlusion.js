f6aa691d483cd0cb616ed3b406605eb93ffe84f8
Glutanimate <glutanimate@users.noreply.github.com>
2015-10-25 09:51:04 +0100

    Update SVG-Edit Image Occlusion extension
    This commit adds hotkeys to the two image occlusion modes ('1' for overlapping, '2' for non-overlapping). It also adds a new 'fit to canvas' button (hotkey: 'c').
    
    Other changes:
    
    - add a callback to fit image content to editor canvas when launching SVG-Edit for the first time in a session
    - prettify code

--- image_occlusion_2/svg-edit/svg-edit-2.7/extensions/ext-image-occlusion.js
+++ image_occlusion_2/svg-edit/svg-edit-2.7/extensions/ext-image-occlusion.js
@@ -1,60 +1,63 @@
 
 svgEditor.addExtension("Image Occlusion (Anki)", function() {
-                return {
-                        name: "Image Occlusion",
-			// For more notes on how to make an icon file, see the source of
-			// the hellorworld-icon.xml
-			svgicons: "extensions/image-occlusion-icon.xml",
-			
-			// Multiple buttons can be added in this array
-			buttons: [
-                                /////////////////////////////////////
-                                // Button for non-overlapping notes
-                                /////////////////////////////////////
-                                {
-				id: "add_notes_non_overlapping", 
-				
-				// This indicates that the button will be added to the "mode"
-				// button panel on the left side
-				type: "mode", 
-				
-				// Tooltip text
-				title: "Add notes with non overlapping occlusions", 
-				
-				// Events
-				events: {
-					"click": function() {
-						// The action taken when the button is clicked on.
-						// For "mode" buttons, any other button will 
-						// automatically be de-pressed.
-						var svg_contents = svgCanvas.svgCanvasToString();
-                                                pyObj.add_notes_non_overlapping(svg_contents);
-                                                }
-                                        }
-                                },
-                                /////////////////////////////////////
-                                // Button for overlapping notes
-                                /////////////////////////////////////
-                                {
-				id: "add_notes_overlapping", 
-				
-				// This indicates that the button will be added to the "mode"
-				// button panel on the left side
-				type: "mode", 
-				
-				// Tooltip text
-				title: "Add notes with overlapping occlusions", 
-				
-				// Events
-				events: {
-					"click": function() {
-						// The action taken when the button is clicked on.
-						// For "mode" buttons, any other button will 
-						// automatically be de-pressed.
-						var svg_contents = svgCanvas.svgCanvasToString();
-                                                pyObj.add_notes_overlapping(svg_contents);
-					}
-				}}
-                        ],
+      return {
+        name: "Image Occlusion",
+  			svgicons: "extensions/image-occlusion-icon.xml",
+  			buttons: [
+        /////////////////////////////////////
+        // Button for non-overlapping notes
+        /////////////////////////////////////
+        {
+  				id: "add_notes_non_overlapping",
+  				type: "mode",
+  				title: "Add notes with non overlapping occlusions",
+          key: "1",
+  				events: {
+  					"click": function() {
+  						// The action taken when the button is clicked on.
+  						// For "mode" buttons, any other button will
+  						// automatically be de-pressed.
+  						var svg_contents = svgCanvas.svgCanvasToString();
+              pyObj.add_notes_non_overlapping(svg_contents);
+            }
+          }
+        },
+        /////////////////////////////////////
+        // Button for overlapping notes
+        /////////////////////////////////////
+        {
+  				id: "add_notes_overlapping",
+  				type: "mode",
+  				title: "Add notes with overlapping occlusions",
+          key: "2",
+  				events: {
+  					"click": function() {
+  						// The action taken when the button is clicked on.
+  						// For "mode" buttons, any other button will
+  						// automatically be de-pressed.
+  						var svg_contents = svgCanvas.svgCanvasToString();
+              pyObj.add_notes_overlapping(svg_contents);
+  					}
+				  }
+        },
+        /////////////////////////////////////
+        // Button for zoom to canvas
+        /////////////////////////////////////
+        {
+          id: "set_zoom_canvas",
+          type: "mode",
+          title: "Fit image to canvas",
+          key: "C",
+          events: {
+            "click": function() {
+              svgCanvas.zoomChanged('', 'canvas');
+            }
+          }
+        }
+      ],
+      // set zoom to "fit to canvas" after loading extension
+      callback: function() {
+        svgCanvas.zoomChanged('', 'canvas');
+      }
 		};
 });
