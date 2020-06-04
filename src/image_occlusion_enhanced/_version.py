61e76f4d209219243e409a7fe5c4a1b1ad03d2c1
Glutanimate <glutanimate@users.noreply.github.com>
2020-06-05 00:58:59 +0200

    Unify all copyright headers

--- src/image_occlusion_enhanced/_version.py
+++ src/image_occlusion_enhanced/_version.py
@@ -2,7 +2,7 @@
 
 # Image Occlusion Enhanced Add-on for Anki
 #
-# Copyright (C) 2016-2019  Aristotelis P. <https://glutanimate.com/>
+# Copyright (C) 2016-2020  Aristotelis P. <https://glutanimate.com/>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
