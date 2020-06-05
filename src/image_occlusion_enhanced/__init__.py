8a926bdbc8d6ad5b53901012237b3b80cf1017a9
Glutanimate <glutanimate@users.noreply.github.com>
2020-06-05 02:27:04 +0200

    Initialize libaddon, adding typing support

--- src/image_occlusion_enhanced/__init__.py
+++ src/image_occlusion_enhanced/__init__.py
@@ -29,6 +29,39 @@
 #
 # Any modifications to this file must keep this entire header intact.
 
-from .main import setup_addon
 
-setup_addon()
+from ._version import __version__  # noqa: F401
+
+from .libaddon import maybeVendorTyping
+
+maybeVendorTyping()
+
+
+def initialize_addon():
+    """Initializes add-on after performing a few checks
+    
+    Allows more fine-grained control over add-on execution, which can
+    be helpful when implementing workarounds for Anki bugs (e.g. the module
+    import bug present in all Anki 2.1 versions up to 2.1.14)
+    """
+
+    from .libaddon import checkFor2114ImportError
+    from .consts import ADDON
+
+    if not checkFor2114ImportError(ADDON.NAME):
+        return False
+
+    from .consts import ADDON
+    from .libaddon.consts import setAddonProperties
+
+    setAddonProperties(ADDON)
+
+    # from .libaddon.debug import maybeStartDebugging
+
+    # maybeStartDebugging()
+
+    from .main import setup_main
+
+    setup_main()
+
+initialize_addon()
