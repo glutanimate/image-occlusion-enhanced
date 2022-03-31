a35ac2f3a13f72b082de153518ce76808aaf33c5
Glutanimate <glutanimate@users.noreply.github.com>
2022-03-31 19:51:59 +0200

    Access fields via NoteEditorAPI

--- src/image_occlusion_enhanced/web/editor.js
+++ src/image_occlusion_enhanced/web/editor.js
@@ -1,23 +1,17 @@
 const NoteEditor = require("anki/NoteEditor");
 
 class ImageOcclusion {
-  // markIdField(index) {
-  //   const idField = NoteEditor.instances[0].fields[index];
-  //   if (!idField) {
-  //     return;
-  //   }
-  //   idField.element.then((element) => {
-  //     {
-  //       element.classList.add("ionote-field-id");
-  //     }
-  //   });
-  // }
-
-  hideIdFieldByIndex(index) {
-    const fields = document.getElementsByClassName("editor-field");
-    fields[index].classList.add("ionote-field-id");
+  markIdField(index) {
+    const idField = NoteEditor.instances[0].fields[index];
+    if (!idField) {
+      return;
+    }
+    idField.element.then((element) => {
+      {
+        element.classList.add("ionote-field-id");
+      }
+    });
   }
 }
 
 globalThis.imageOcclusion = new ImageOcclusion();
-
